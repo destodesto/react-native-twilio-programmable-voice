cbfa8ea48c9793c62ba9bc7cc2955a22a38e7f1f
Desto Relente, Jr <desto.relente@mph-intl.com>
2020-01-30 12:06:57 +0800

    enabled setting of call ringtone (Android)

--- android/src/main/java/com/hoxfon/react/RNTwilioVoice/SoundPoolManager.java
+++ android/src/main/java/com/hoxfon/react/RNTwilioVoice/SoundPoolManager.java
@@ -3,17 +3,23 @@ package com.hoxfon.react.RNTwilioVoice;
 import android.content.Context;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
+import android.content.SharedPreferences;
 import android.net.Uri;
 
 public class SoundPoolManager {
 
+    public static final String PREFERENCES_KEY_NOTIFICATION_SETTINGS = "group.com.sosearch.sos";
+    private Context context;
     private boolean playing = false;
     private static SoundPoolManager instance;
     private Ringtone ringtone = null;
+    private final SharedPreferences notificationSettingsDefaultPersistence;
+
 
     private SoundPoolManager(Context context) {
-        Uri ringtoneSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-        ringtone = RingtoneManager.getRingtone(context, ringtoneSound);
+        
+        this.context = context;
+        this.notificationSettingsDefaultPersistence = context.getSharedPreferences(PREFERENCES_KEY_NOTIFICATION_SETTINGS, Context.MODE_PRIVATE);
     }
 
     public static SoundPoolManager getInstance(Context context) {
@@ -24,6 +30,24 @@ public class SoundPoolManager {
     }
 
     public void playRinging() {
+        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+        String soundName = notificationSettingsDefaultPersistence.getString("notifPhoneRingSound", "iphone.mp3");
+        int resId;
+        
+        if (!soundName.equals("iphone.mp3")) {
+            // String soundName = bundle.getString("soundName");
+            if (context.getResources().getIdentifier(soundName, "raw", context.getPackageName()) != 0) {
+                resId = context.getResources().getIdentifier(soundName, "raw", context.getPackageName());
+            } else {
+                soundName = soundName.substring(0, soundName.lastIndexOf('.'));
+                resId = context.getResources().getIdentifier(soundName, "raw", context.getPackageName());
+            }
+
+            soundUri = Uri.parse("android.resource://" + context.getPackageName() + "/" + resId);
+        }
+
+        ringtone = RingtoneManager.getRingtone(context, soundUri);
+
         if (!playing) {
             ringtone.play();
             playing = true;
